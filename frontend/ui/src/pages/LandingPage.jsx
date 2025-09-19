1e000703effb7d2de355ed8326842b0d0bb0e9c3
vivekkannamreddi <vivekkannamreddi@gmail.com>
2025-09-19 16:14:16 +0530

    updated landing page

--- frontend/ui/src/pages/LandingPage.jsx
+++ frontend/ui/src/pages/LandingPage.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import mobile from '../assets/mobiles.png'
+import { Link } from 'react-router-dom'
 
 export default function LandingPage() {
   return (
-    <div>
-        hello
+    <div className='LandingPageContainer'>
+        <nav>
+          <div className="navHeader">
+            <h2>EchoLink</h2>
+          </div>
+          <div className="navList">
+            <p>Guest</p>
+            <p>Register</p>
+            <div role='button'>
+              <p>Login</p>
+            </div>
+          </div>
+        </nav>
+
+        <div className="LandingMainContainer">
+          <div>
+            <h1><span style={{color:" rgb(245, 119, 0)"}}>Connect</span> with your loved ones</h1>
+            <p>Cover distance by EchoLink</p>
+            <div role='button'>
+              <Link to={"/home"}>Get Started</Link>
+            </div>
+          </div>
+          <div>
+            <img src={mobile} alt="" />
+          </div>
+        </div>
     </div>
   )
 }
